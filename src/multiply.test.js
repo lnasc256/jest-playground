73971b46d9b7d42041ecda4c507077b833baf25d
lucas nascimento <iam@lucasnascimen.to>
2022-06-05 13:39:43 -0300

    typos fixed

--- src/multiply.test.js
+++ src/multiply.test.js
@@ -1,9 +1,9 @@
 const multiply = require('./multiply');
 
-test('adds 3 * 2 to equal 6', () => {
+test('3 * 2 to equal 6', () => {
   expect(multiply(3, 2)).toBe(6);
 });
 
-test('adds 6 * 3 to equal 18', () => {
+test('6 * 3 to equal 18', () => {
   expect(multiply(6, 3)).toBe(18);
 });
